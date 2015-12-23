7e6620b57c294b2c9040a6c986a459343398d505
liuchang8am <liuchang8am@gmail.com>
2015-12-23 18:50:14 +0800

    flickr8k: now loads .h5 and .json file, and can present results

--- data/flickr8k/flickr8k.lua
+++ data/flickr8k/flickr8k.lua
@@ -4,7 +4,6 @@
 
 --require("mobdebug").start()
 require 'hdf5' -- read .h5 file format
-require 'cjson' -- read .json file format
 require 'dp'
 require 'torchx' -- for paths.indexdir
 require 'image'
@@ -28,7 +27,7 @@ function prepare_flickr8k(datapath)
     --	->	    each containes a sequence of numbers, representing the numbered word 
     -- #TODO: add assertion to make sure file exists
     print ('Loading flickr8k data.h5 file ...')
-    local h5_file = hdf5.open(datapath)
+    local h5_file = hdf5.open(datapath..'/'..'data.h5')
     local images = h5_file:read('/images'):all() -- read all 100 images, access via images[i] for image i
     local sentences = h5_file:read('/labels'):all() -- read all 100 sentences, access via sentences[i] for image i
     h5_file:close()
@@ -37,20 +36,43 @@ function prepare_flickr8k(datapath)
     -- pre-prepared with neuraltalk2/prepo.py
 
     -- data.json file format:
-    --	-> 'images': containes info of the 100 images
+    --	-> 'images': containes info of the 100 images, 100 x {'flie_path', 'split'}
     --	     -> 'file_path': string, the filename, eg. '1597557856_30640e0b43.jpg'
     --	     -> 'split': string, 'train', 'validation', or 'test' split
     --	-> 'ix_to_word': the mapping from number to word, eg. 101 is 'cigarettes' 
     -- #TODO: add assertion to make sure file exists
     print ('Loading flickr8k data.json file ...')
-    local json_file = io.open('data.json', 'r'):read()
+    local json_file = io.open(datapath..'/'..'data.json', 'r')
+    local text = json_file:read()
+    local cjson = require 'cjson' -- read .json file format
+    local json_file_contents = cjson.decode(text)
+    local ix_to_word = json_file_contents.ix_to_word
+    local images_info = json_file_contents.images
+    json_file:close()
+
+    -- put the images, sentences, images_info, and ix_to_word all together into a flickr8k dataset
+    local flickr8k = {images, sentences, images_info, ix_to_word}
+    -- access examples:
+    require 'image'
+    image.display(flickr8k[1][1]) -- first [1]: the images field of flickr8k; second [1]: the first image of the images
+    for i = 1, (flickr8k[2][1]):size()[1] do -- :size()[1] makes sure it is a number rather than a torch.LongStorage of size 1
+	io.write (flickr8k[2][1][i], ' ')
+    end
+    --print (flickr8k[2][1]) -- the sentence of image [1]
+    print (flickr8k[3][1]['file_path']) 
+    print (flickr8k[3][1]['split']) 
+    print (flickr8k[4]["120"])
 
     -- let's try to print the caption sentence for image[1]
-
-
+    for i = 1, (flickr8k[2][1]):size()[1]-1 do
+	local word_int = flickr8k[2][1][i] -- word i in sentence of image[1]
+	local word_str = string.format("%d", word_int) -- convert int to string, using string.format function
+	local word = flickr8k[4][word_str]
+	io.write (word, ' ')
+    end
+    print ('.')
     return h5_file
 end
 
 
-data = prepare_flickr8k('data.h5')
-print (A)
+data = prepare_flickr8k('/home/lc/codes/ImageSayWithAttention/data/flickr8k/')
