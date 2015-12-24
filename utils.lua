e68ee35cb9cd3b2f78f505a5fe6d4592ddb06e55
liuchang8am <liuchang8am@gmail.com>
2015-12-24 14:50:13 +0800

    rm utils.lua

--- utils.lua
+++
@@ -1,7 +0,0 @@
---- utility functions here ---
-
--- check if file exists
-function file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
-end
